42588eddf9b7d4fa63a28fd0da74b9852f073938
tndrm <p-kalenik@ya.ru>
2016-04-26 17:49:32 +0300

    Added files via upload

---
+++ todos.js
@@ -0,0 +1,64 @@
+var all = [
+		{ checkbox: 'checked', value: 'wake up' },
+		{ checkbox: '', value: 'survaive' },
+		{ checkbox: '', value: 'back to sleep' }
+	]
+var mode = 'all';
+
+function printCases(mode) { ///???????????????
+	var cases = (mode === 'all') ? showAll() :
+			 	(mode === 'completed') ? showCompleted() : showActive();	
+	var closeButton = ''
+	var allCases = '';
+	var colorCase = '';
+	for (var id = 0; id <= cases.length - 1; id++) {
+		colorCase = 'rgb(' + (192 - (id * 16)) + ',' + (232 - (id * 16)) + ',' + (248 - (id * 16)) + ')';
+		closeButton = '<input type="button" onclick="closeCase(\''+ id +'\')" name="close" id = "close"></input>'
+		allCases += '<div id = "color-box" style = "background-color:' + colorCase +'"><input type="checkbox" onchange="checkCase(this)"' + 'id = \"' + id + '\"'+ cases[id].checkbox + '><label ' + 'for=\"' + id +'\"><span></span>' + cases[id].value + '</label></input>' + closeButton + '</div>'
+	};
+	document.getElementById('allCases').innerHTML = allCases
+}
+
+function addToList(form){
+	if (form.newCase.value) { 
+		all[all.length] = {checkbox: '', value: form.newCase.value};
+	};
+	printCases(mode);
+
+}
+
+function checkCase(currentCase) {
+	all[currentCase.id].checkbox === '' ? all[currentCase.id].checkbox = 'checked' : all[currentCase.id].checkbox = '';
+	printCases(mode);
+}
+
+function showActive() {
+	mode = 'active';
+	var active = all.filter(function(element) {
+		return (element.checkbox == '')
+	});
+	return active
+}
+
+function showAll() {
+	mode = 'all'
+	return all
+}
+
+function showCompleted() {
+	mode = 'completed';
+	var completed = all.filter(function(element) {
+		return (element.checkbox == 'checked')
+	});
+	return completed
+}
+function clearCompleted(){
+	all = all.filter(function(element){
+		return (element.checkbox ==='')
+	})
+	printCases(mode)
+}
+function closeCase(id) {
+	all.splice(id, 1)
+	printCases(mode)
+}
